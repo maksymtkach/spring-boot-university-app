ae3ec18a5dd6add111ce5a255b40f67c36ad28f8
Maksym Tkach <maksym.tkach@student.kdg.be>
2025-05-09 18:34:30 +0200

    Initial commit

---
+++ src/main/java/org/example/simpleuniversity/SimpleUniversityApplication.java
@@ -0,0 +1,13 @@
+package org.example.simpleuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleUniversityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/simpleuniversity/SimpleUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.simpleuniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleUniversityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
